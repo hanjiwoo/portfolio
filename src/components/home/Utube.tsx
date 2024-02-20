8b146a99d34d382fd755b31a81009a2942eca715
hanjiwoo <hanmingyu3@naver.com>
2024-02-20 10:18:27 +0900

    2/20일 push(4)

--- src/components/home/Utube.tsx
+++ src/components/home/Utube.tsx
@@ -1,13 +1,21 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Utube() {
+  const [videoGO, setVideoGo] = useState(false);
+  useEffect(() => {
+    setVideoGo(true);
+  }, []);
   return (
     <div className="bg-red-300">
-      <video width="640" height="360" controls>
-        <source src="/미란이영상.mp4" type="video/mp4" />
-      </video>
-      <h1>고양이 구경하세요</h1>
+      {videoGO && (
+        <>
+          <video width="640" height="360" controls>
+            <source src="/미란이영상.mp4" type="video/mp4" />
+          </video>
+          <h1>고양이 구경하세요</h1>
+        </>
+      )}
     </div>
   );
 }
