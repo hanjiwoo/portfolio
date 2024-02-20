525309b615df0fe95a1a7ca6cfcee69ae97f3626
hanjiwoo <hanmingyu3@naver.com>
2024-02-20 10:32:13 +0900

    2/20일 push(5)

--- src/components/home/Utube.tsx
+++ src/components/home/Utube.tsx
@@ -2,20 +2,20 @@
 import React, { useEffect, useState } from "react";
 
 export default function Utube() {
-  const [videoGO, setVideoGo] = useState(false);
-  useEffect(() => {
-    setVideoGo(true);
-  }, []);
+  // const [videoGO, setVideoGo] = useState(false);
+  // useEffect(() => {
+  //   setVideoGo(true);
+  // }, []);
   return (
     <div className="bg-red-300">
-      {videoGO && (
-        <>
-          <video width="640" height="360" controls>
-            <source src="/미란이영상.mp4" type="video/mp4" />
-          </video>
-          <h1>고양이 구경하세요</h1>
-        </>
-      )}
+      {/* {videoGO && ( */}
+      <>
+        <video width="640" height="360" controls>
+          <source src="/미란이영상.mp4" type="video/mp4" />
+        </video>
+        <h1>고양이 구경하세요</h1>
+      </>
+      {/* )} */}
     </div>
   );
 }
--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
@@ -17,7 +17,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
 export default app;
